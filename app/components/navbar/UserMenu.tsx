a9338dd6c6ed0db80038f8c00481a3ecf20d2766
Mozahid RANA <mozahidpic@gmail.com>
2024-03-01 01:31:38 +1100

    properties page and delete properties

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -64,7 +64,7 @@ export const UserMenu: React.FC<UserManuProps> = ({ currentUser }) => {
                 <MenuItem onClick={() => router.push("/trips")} label="My trips" />
                 <MenuItem onClick={() => router.push("/favourites")} label="My favorites" />
                 <MenuItem onClick={() => router.push("/reservations")} label="My reservations" />
-                <MenuItem onClick={() => {}} label="My properties" />
+                <MenuItem onClick={() => router.push("/properties")} label="My properties" />
                 <MenuItem onClick={rentModal.onOpen} label="Airbnb My home" />
                 <hr />
                 <MenuItem onClick={() => {signOut()}} label="Logout" />
