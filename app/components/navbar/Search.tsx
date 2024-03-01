7d13c782a2251127c08788222348392641bb5387
Mozahid RANA <mozahidpic@gmail.com>
2024-03-01 23:12:34 +1100

    Search page updated

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,9 +1,57 @@
 'use client';
-import { useSearchModal } from '@/app/hooks/useSearchModal';
+
 import { BiSearch } from 'react-icons/bi';
+
+import { useCountries } from '@/app/hooks/useCountries';
+import { useSearchModal } from '@/app/hooks/useSearchModal';
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
+import { differenceInDays } from 'date-fns';
 
 const Search = () => {
   const searchModal = useSearchModal();
+  const params = useSearchParams(); // react hook
+  const { getByValue } = useCountries();
+
+  const locationValue = params?.get('locationValue');
+  const startDate = params?.get('startDate');
+  const endDate = params?.get('endDate');
+  const guestCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+    return 'Anywhere';
+  }, [getByValue, locationValue]);
+
+  // Duration label
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+    return 'Any week';
+  }, [startDate, endDate]);
+
+  // Guest label
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
+
   return (
     <div
       onClick={searchModal.onOpen}
@@ -25,12 +73,12 @@ const Search = () => {
         items-center
         justify-between"
       >
-        <div className="text-sm font-semibold px-6">Anywhere</div>
+        <div className="text-sm font-semibold px-6">{locationLabel}</div>
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-          Any Week
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <BiSearch size={18} />
           </div>
