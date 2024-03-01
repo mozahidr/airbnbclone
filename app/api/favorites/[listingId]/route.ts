38a8e31c24f927a0a9e9207ce45a14ce6f2288e9
Mozahid RANA <mozahidpic@gmail.com>
2024-03-01 23:40:10 +1100

    folder name change to favorites

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
-import { json } from "stream/consumers";
 
 interface IParams {
     listingId?: string;
